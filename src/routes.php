d9a1236b8bdce15c6ada9d147ce376ace81973a1
Devin Matte <devin@devinmatte.com>
2019-02-16 19:07:19 -0500

    Clearing demo routes

--- src/routes.php
+++ src/routes.php
@@ -3,26 +3,6 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-// GET Request
-$app->get('/', function (Request $request, Response $response) {
-    return $response->write("Hello World!");
-});
-
-// POST Request
-$app->post('/', function (Request $request, Response $response) {
-    return $response->write("Got a POST request at /");
-});
-
-// PUT Request
-$app->put('/', function (Request $request, Response $response) {
-    return $response->write("Got a PUT request at /");
-});
-
-// DELETE Request
-$app->delete('/', function (Request $request, Response $response) {
-    return $response->write("Got a DELETE request at /");
-});
-
 $app->group('/spotify', function () use ($app) {
 
     $app->get('/auth', function (Request $request, Response $response) {
